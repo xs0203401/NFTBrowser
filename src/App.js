75a9f776b0db3b706f1fd4df2af17879c2e02f5c
Yao <henryliuyaohuang@gmail.com>
2023-07-10 21:05:47 -0500

    NFT browser 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "antd";
 import { searchNFTs } from "./utils";
+import Moralis from "moralis";
 
 function App() {
-  const handleButtonClick = () => {
-    searchNFTs("car");
+  const handleButtonClick = async () => {
+    await Moralis.start({
+      apiKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6Ijk2YzQyMjYwLTRiMDQtNDQzOS05YTNjLTIyMzg4Y2ZjNDA5NiIsIm9yZ0lkIjoiMzQ3MzgzIiwidXNlcklkIjoiMzU3MDc4IiwidHlwZUlkIjoiZWNlMDFmZjQtODZlOS00YzY0LWIwMDItZWE5MGM3ZGQxMzU4IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODg4NzA5MTMsImV4cCI6NDg0NDYzMDkxM30.KCEXcn2IwTfVOVsFMBW8Aryx7lMHy0hZipDiBiMsqnk",
+    });
+
+    await Moralis.EvmApi.nft.searchNFTs({
+      chain: "eth",
+      format: "decimal",
+      q: "car",
+      filter: "name",
+      limit: 12,
+    });
   };
 
   return (
