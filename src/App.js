42262a04de2befcf9a5f740113e647fc5bb192e9
Yao <henryliuyaohuang@gmail.com>
2023-07-11 01:25:17 -0500

    Go back to the starting point

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import Moralis from "moralis";
 
 function App() {
   const handleButtonClick = async () => {
-    await Moralis.start({
-      apiKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6Ijk2YzQyMjYwLTRiMDQtNDQzOS05YTNjLTIyMzg4Y2ZjNDA5NiIsIm9yZ0lkIjoiMzQ3MzgzIiwidXNlcklkIjoiMzU3MDc4IiwidHlwZUlkIjoiZWNlMDFmZjQtODZlOS00YzY0LWIwMDItZWE5MGM3ZGQxMzU4IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODg4NzA5MTMsImV4cCI6NDg0NDYzMDkxM30.KCEXcn2IwTfVOVsFMBW8Aryx7lMHy0hZipDiBiMsqnk",
-    });
-
-    await Moralis.EvmApi.nft.searchNFTs({
-      chain: "eth",
-      format: "decimal",
-      q: "car",
-      filter: "name",
-      limit: 12,
-    });
+    searchNFTs("car");
   };
 
   return (
