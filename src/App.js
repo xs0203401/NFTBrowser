d3dec681eced88df1364f9ce111cf8e003299926
Yao <henryliuyaohuang@gmail.com>
2023-07-11 01:36:05 -0500

    useState => loading + searchText

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
+import { Button, Input, Layout, message } from "antd";
 import { searchNFTs } from "./utils";
-import Moralis from "moralis";
+import { Content, Header } from "antd/lib/layout/layout";
+import { useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
   const handleButtonClick = async () => {
     searchNFTs("car");
   };
 
+  const handleSearch = async () => {
+    try {
+      setLoading(true);
+      const data = await searchNFTs(searchText);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+    }
+  };
+
   return (
-    <div className="App">
-      <Button onClick={handleButtonClick}>Test</Button>
-    </div>
+    // <div className="App">
+    //   <Button onClick={handleButtonClick}>Test</Button>
+    // </div>
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div style={{ fontSize: 16, fontWeight: 6, color: "white" }}>
+          NFT Browser
+        </div>
+      </Header>
+      <Content
+        style={{ height: "calc(100% - 64px)", padding: 20, overflow: "auto" }}
+      >
+        <Input.Group compact style={{ width: 500 }}>
+          <Input
+            style={{
+              width: "calc(100% - 200px)",
+            }}
+            defaultValue="https://ant.design"
+          />
+          <Button type="primary" onClick={handleSearch}>
+            Submit
+          </Button>
+        </Input.Group>
+      </Content>
+    </Layout>
   );
 }
 
