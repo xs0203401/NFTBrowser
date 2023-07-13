f28a6033f5ebd1916096d33f5ccac472440749c1
Yao <henryliuyaohuang@gmail.com>
2023-07-12 22:56:01 -0500

    Button + Modal + Table

--- src/components/ContractNfts.js
+++ src/components/ContractNfts.js
@@ -1,7 +1,31 @@
-import { Table } from "antd";
+import { Button, Modal, Table } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+
+const columns = [];
+
+const data = [];
 
 const ContractNfts = () => {
-  return <Table columns={columns} dataSource={data} />;
+  return (
+    <>
+      <Button
+        style={{ border: "none" }}
+        size="large"
+        shape="circle"
+        icon={<InfoCircleOutlined />}
+        onClick={() => setModalOpen(true)}
+      />
+      <Modal
+        width={1000}
+        title="NFT(s) List"
+        destroyOnClose
+        open={modalOpen}
+        footer={null}
+      >
+        <Table columns={columns} dataSource={data} />
+      </Modal>
+    </>
+  );
 };
 
 export default ContractNfts;
